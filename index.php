f78eda91c32c72deb09d5f5aef4311a8d60524dc
井上沙耶 <>
2018-07-21 00:09:52 +0800

    post.phpとつなげた

--- index.php
+++ index.php
@@ -42,7 +42,7 @@
         </div>
         <div class="navbar-collapse collapse">
           <ul class="nav navbar-nav navbar-right">
-            <li class="active"><a href="post.html">Post photos</a></li>
+            <li class="active"><a href="post.php">Post photos</a></li>
           </ul>
         </div><!--/.nav-collapse -->
       </div>
