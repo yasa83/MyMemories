c3847664c6de656902c3daba92ea23ecc1bbe3f2
井上沙耶 <>
2018-07-21 17:10:32 +0800

    投稿が完了したのを知らせたい画面

---
+++ thanks.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>My Memories</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="assets/css/bootstrap.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="assets/css/main.css" rel="stylesheet">
+    <link href="assets/css/font-awesome.min.css" rel="stylesheet">
+
+    <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
+    <script src="assets/js/chart.js"></script>
+</head>
+<body style="margin-top: 60px">
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-8 col-xs-offset-2 thumbnail">
+                <h2 class="text-center content_header">投稿完了</h2>
+                <div class="text-center">
+                    <p>写真のアップデートが完了しました。</p>
+                    <a href="index.php" class="btn btn-info">一覧に戻る</a>
+                </div>
+            </div>
+        </div>
+    </div>
+<script src="assets/js/jquery-3.1.1.js"></script>
+<script src="assets/js/jquery-migrate-1.4.1.js"></script>
+<script src="assets/js/bootstrap.js"></script>
+</body>
+</html>
