53b1b730f0310a385d46f1244c702c92d063f870
井上沙耶 <>
2018-07-30 23:46:06 +0800

    完成

--- thanks.php
+++ thanks.php
@@ -1,23 +1,61 @@
 <!DOCTYPE html>
-<html lang="ja">
+<html lang="en">
 <head>
-  <meta charset="utf-8">
-  <title>Learn SNS</title>
-  <link rel="stylesheet" type="text/css" href="../assets/css/bootstrap.css">
-  <link rel="stylesheet" type="text/css" href="../assets/font-awesome/css/font-awesome.css">
-  <link rel="stylesheet" type="text/css" href="../assets/css/style.css">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>My Memories</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="assets/css/bootstrap.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="assets/css/main.css" rel="stylesheet">
+    <link href="assets/css/font-awesome.min.css" rel="stylesheet">
+
+    <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
+    <script src="assets/js/chart.js"></script>
 </head>
-<body style="margin-top: 60px">
+
+<body>
+    <!-- Fixed navbar -->
+    <div class="navbar navbar-default navbar-fixed-top">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href=""><i class="fa fa-heart" style="color: #fff;"></i></a>
+            </div>
+        </div>
+    </div>
+    <!-- ここから -->
   <div class="container">
     <div class="row">
       <div class="col-xs-8 col-xs-offset-2 thumbnail">
         <h2 class="text-center content_header">アカウント作成完了</h2>
+        <a href="signin.php" style="float: right; padding-top: 6px;" class="text-success">サインイン</a>
       </div>
     </div>
-    </div>
   </div>
-  <script src="../assets/js/jquery-3.1.1.js"></script>
-  <script src="../assets/js/jquery-migrate-1.4.1.js"></script>
-  <script src="../assets/js/bootstrap.js"></script>
+
+
+  <footer>
+    <div id="f">
+        <div class="container">
+            <div class="row">
+                <p>I <i class="fa fa-heart"></i> Cubu.</p>
+            </div>
+        </div>
+    </div>
+  </footer>
+        <script src="../assets/js/jquery-3.1.1.js"></script>
+        <script src="../assets/js/jquery-migrate-1.4.1.js"></script>
+        <script src="../assets/js/bootstrap.js"></script>
 </body>
 </html>
