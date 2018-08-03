5cb56444f70bfbb83f9324d38b87497d0f2e6a9a
井上沙耶 <>
2018-08-03 09:04:47 +0800

    サインアウト

---
+++ signout.php
@@ -0,0 +1,13 @@
+<?php
+
+    session_start();
+
+    // SESSION変数の破棄
+    $_SESSION = [];
+
+    // サーバー内の$_SESSION変数のクリア
+    session_destroy();
+
+    // signin.phpへの移動
+    header("Location: signin.php");
+    exit();
